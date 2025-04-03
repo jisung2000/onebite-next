c23bd83a9afe3d190ea5fab8e3eb92f6f6a596db
jisung <swwegjisung@gmail.com>
2025-04-04 00:27:50 +0900

    Feat: Home 페이지에 getServerSideProps 추가하여 SSR 데이터 구조 도입

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,21 @@ import style from "./index.module.css"
 import { ReactNode } from "react";
 import books from '@/mock/books.json'
 import BookItem from "@/componenets/book-item";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
+ export const getServerSideProps = () => {
+  // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+
+  const data ="hello";
+  return {
+    props:{
+      data,
+    },
+  };
+ };
+
+export default function Home({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data);
   return <div className={style.container}>
     <section>
       <h3>지금 추천하는 도서</h3>
