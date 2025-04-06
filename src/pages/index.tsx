07a41c73516721b632509e5ebab59e0634c8eae7
jisung <swwegjisung@gmail.com>
2025-04-06 19:16:51 +0900

    Feat: 모든 페이지에 getServerSideProps 적용하여 서버사이드 렌더링 기반 데이터 로딩 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,43 @@
 import SearchableLayout from "@/componenets/searchable-layout";
 import style from "./index.module.css"
 import { ReactNode } from "react";
-import books from '@/mock/books.json'
 import BookItem from "@/componenets/book-item";
 import { InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
- export const getServerSideProps = () => {
+ export const getServerSideProps = async () => {
   // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(), fetchRandomBooks(),
+  ]);
 
-  const data ="hello";
   return {
     props:{
-      data,
+      allBooks,
+      recoBooks,
     },
   };
  };
 
-export default function Home({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log(data);
+export default function Home({ 
+  allBooks, 
+  recoBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps
+  >) {
+  console.log(allBooks);
   return <div className={style.container}>
     <section>
       <h3>지금 추천하는 도서</h3>
-      {books.map((book) =>(
-        <BookItem key={books.id}{...book}/>
+      {recoBooks.map((book) =>(
+        <BookItem key={book.id} {...book}/>
           ))}
     </section>
     <section>
       <h3>등록된 모든 도서</h3>
+      {allBooks.map((book) =>(
+        <BookItem key={book.id} {...book}/>
+          ))}
     </section>
   </div>;
 }
