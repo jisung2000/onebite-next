a8792d5c59675cea77c7c2d25d95965c3916039e
jisung <swwegjisung@gmail.com>
2025-04-24 01:50:21 +0900

    Feat : 데이터 캐시

--- section04/src/app/layout.tsx
+++ section04/src/app/layout.tsx
@@ -1,6 +1,24 @@
 import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
+import { BookData } from "@/types";
+
+async function Footer(){
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
+  if(!response){
+    return <div>제작 @winterlood</div>
+  }
+
+  const books: BookData[] = await response.json();
+  const bookCount = books.length;
+
+  return <footer>
+    <div>제작 @winterlood</div>
+    <div>등록된 도서는 총 {bookCount} 권 입니다.</div>
+  </footer>
+}
 
 export default function RootLayout({
   children,
@@ -15,7 +33,7 @@ export default function RootLayout({
             <Link href={"/"}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @winterlood</footer>
+          <Footer/>
         </div>
       </body>
     </html>
