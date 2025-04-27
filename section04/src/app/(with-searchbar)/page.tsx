f842af350f9e05262d3fbd68ba3fa58cee64c2ce
jisung <swwegjisung@gmail.com>
2025-04-27 12:26:58 +0900

    Refactor: refactor code line

--- section04/src/app/(with-searchbar)/page.tsx
+++ section04/src/app/(with-searchbar)/page.tsx
@@ -3,7 +3,8 @@ import style from "./page.module.css";
 import { BookData } from "@/types";
 
 async function AllBooks(){
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
     {cache:"no-store"}
   );
   if(!response.ok) {
